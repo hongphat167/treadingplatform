362d01f666af4971629155069671b13d0bca01a1
hongphat <hongphat1671998@gmail.com>
2024-07-28 17:08:56 +0700

    remove code unused

--- treadingplatform_BE/coin/src/main/java/com/treading/coin/config/VnPayConfig.java
+++ treadingplatform_BE/coin/src/main/java/com/treading/coin/config/VnPayConfig.java
@@ -36,38 +36,6 @@ public class VnPayConfig {
   @Value("${vnpay.apiUrl}")
   private String vnp_ApiUrl;
 
-//  public String md5(String message) {
-//    String digest = null;
-//    try {
-//      MessageDigest md = MessageDigest.getInstance("MD5");
-//      byte[] hash = md.digest(message.getBytes("UTF-8"));
-//      StringBuilder sb = new StringBuilder(2 * hash.length);
-//      for (byte b : hash) {
-//        sb.append(String.format("%02x", b & 0xff));
-//      }
-//      digest = sb.toString();
-//    } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
-//      digest = "";
-//    }
-//    return digest;
-//  }
-
-//  public String sha256(String message) {
-//    String digest = null;
-//    try {
-//      MessageDigest md = MessageDigest.getInstance("SHA-256");
-//      byte[] hash = md.digest(message.getBytes("UTF-8"));
-//      StringBuilder sb = new StringBuilder(2 * hash.length);
-//      for (byte b : hash) {
-//        sb.append(String.format("%02x", b & 0xff));
-//      }
-//      digest = sb.toString();
-//    } catch (UnsupportedEncodingException | NoSuchAlgorithmException ex) {
-//      digest = "";
-//    }
-//    return digest;
-//  }
-
   public String hashAllFields(Map<String, String> fields) {
     List<String> fieldNames = new ArrayList<>(fields.keySet());
     Collections.sort(fieldNames);
@@ -106,19 +74,6 @@ public class VnPayConfig {
       return "";
     }
   }
-
-//  public String getIpAddress(HttpServletRequest request) {
-//    String ipAdress;
-//    try {
-//      ipAdress = request.getHeader("X-FORWARDED-FOR");
-//      if (ipAdress == null) {
-//        ipAdress = request.getRemoteAddr();
-//      }
-//    } catch (Exception e) {
-//      ipAdress = "Invalid IP:" + e.getMessage();
-//    }
-//    return ipAdress;
-//  }
 
   public String getRandomNumber(int len) {
     Random rnd = new Random();
